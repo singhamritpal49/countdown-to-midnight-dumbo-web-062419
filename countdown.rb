d9ef31e6694fb3f17da19e82d7fadd44a87dbb4c
Amritpal Singh <singhamritpal49@gmail.com>
2019-05-30 18:28:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  i = 10
+  while i <= number
+    puts "#{i} SECOND(S)!"
+    i -=1
+    break if i == 0
 end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
